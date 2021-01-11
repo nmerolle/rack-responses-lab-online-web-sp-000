537cec85303ebcc0d977b47c1d8aa2b8bfef07f7
Nicholas Merolle <nmerolle11@hotmail.com>
2021-01-11 01:51:33 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,20 @@
+class Application
+ 
+  def call(env)
+    resp = Rack::Response.new
+ 
+    num_1 = Kernel.rand(1..24)
+    
+ 
+    resp.write "#{num_1}\n"
+    
+    if num_1 >= 12 
+      resp.write "Good Afternoon!"
+    else
+      resp.write "Good Morning!"
+    end
+ 
+    resp.finish
+  end
+ 
+end
